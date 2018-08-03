ffa52181ccb6ae688a42203ec0d518163ccca2ca
Sava Vuckovic <vuckovicsava11@gmail.com>
2018-08-03 22:43:53 +0200

    added webpack setup with sass

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: './src/js/index.js'
+  },
+  mode: 'development',
+  output: {
+    filename: '[name]-bundle.js',
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/'
+  },
+  devServer: {
+    contentBase: 'dist',
+    overlay: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].html'
+            }
+          },
+          {
+            loader: 'extract-loader'
+          },
+          {
+            loader: 'html-loader'
+          }
+        ]
+      }
+    ]
+  }
+}
