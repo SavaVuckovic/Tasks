4c62475e48b52e276c3a87498c0938eb8b5b3aad
Sava Vuckovic <vuckovicsava11@gmail.com>
2018-08-06 14:23:13 +0200

    rendering task form

---
+++ src/js/task.js
@@ -0,0 +1,11 @@
+export default class Task {
+  constructor(title, description, dueDate, priority) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.complete = false;
+    this.checklist = [];
+    this.notes = [];
+  }
+}
